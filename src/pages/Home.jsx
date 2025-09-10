16bb1e07b08fd371c139a86310bda73ef2d2835b
vanshaj bansal <bnslgvansh67@gmail.com>
2025-09-10 21:44:49 +0530

    Font colour Changed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
     return (
         <div>
-            <div className='text-white'>This is HOME</div>
+            <div className='text-black'>This is HOME</div>
         </div>
     )
 }
